be10cb255c3383aea3212f3072076da90aad86b0
SviatelykAnna <anna.sviatelyk@gmail.com>
2026-02-01 15:23:24 +0400

    code: add husky

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,6 +8,8 @@ function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
     <div className="text-dark-grey relative">
       <input
         type={type}
+
+        
         data-slot="input"
         className={cn(
           'file:text-foreground placeholder:text-muted-foreground selection:text-primary-foreground dark:bg-input/30 border-input relative h-9 w-full min-w-0 rounded-lg border bg-transparent px-3 py-1 pr-8 text-base shadow-xs transition-[color,box-shadow] outline-none selection:bg-white file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
